993e83eed684c996be12a7a299955ecf3c2208aa
sujata nalage <[sujatanalage22@gmail.com]>
2026-01-08 23:00:58 +0530

    loginregisterworks

---
+++ src/main/java/com/cjc/springbootapp/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.cjc.springbootapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.cjc.springbootapp.model.User;
+import com.cjc.springbootapp.service.UserService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    // LOGIN
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        User loggedUser = userService.login(user.getEmail(), user.getPassword());
+
+        if (loggedUser == null) {
+            return ResponseEntity.status(401).body("Invalid email or password");
+        }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("id", loggedUser.getId());
+        response.put("name", loggedUser.getName());
+        response.put("email", loggedUser.getEmail());
+        response.put("role", loggedUser.getRole());
+
+        return ResponseEntity.ok(response);
+    }
+
+    // REGISTER (Customer / Mentor)
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        return ResponseEntity.ok(userService.register(user));
+    }
+}
